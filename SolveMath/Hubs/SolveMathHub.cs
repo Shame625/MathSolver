108ad727f173afd14d3ffe44335551161d359e90
Branimir <branimir@crnojaje.hr>
2019-06-24 15:35:20 +0200

    Init

---
+++ SolveMath/Hubs/SolveMathHub.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public static class CurrentGame
+    {
+        public static Equation currentEquation;
+
+        public static Equation GetCurrentEquation()
+        {
+            if (currentEquation == null)
+            {
+                return StartNewGame();
+            }
+
+            return currentEquation;
+        }
+
+        public static Equation StartNewGame()
+        {
+            currentEquation = new Equation();
+            return currentEquation;
+        }
+
+        public static bool IsCorrect(int input)
+        {
+            if (input == currentEquation.GetResult())
+                return true;
+            return false;
+        }
+    }
+    public class SolveMath : Hub
+    {
+        public async Task GetCurrentGame()
+        {
+            await Clients.Caller.SendAsync("Equation", CurrentGame.GetCurrentEquation());
+        }
+
+        public async Task SendAnswer(int message)
+        {
+            if(CurrentGame.IsCorrect(message))
+            {
+                CurrentGame.currentEquation.hasWinner = true;
+                CountdownToNewGame();
+            }
+            else if(CurrentGame.currentEquation.hasWinner)
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", "MSG", "Game already finished!");
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", "MSG", "Wrong, try again.");
+            }
+        }
+
+        private async void CountdownToNewGame()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "GAME_RESTARTING", "WE HAVE A WINNER, CORRECT ANSWER WAS: " + CurrentGame.currentEquation.GetResult());
+            int seconds = 5;
+            for(;seconds >= 0; seconds--)
+            {
+                await Clients.All.SendAsync("ReceiveMessage", "GAME", "SERVER: " + seconds);
+                Thread.Sleep(1000);
+            }
+            CurrentGame.StartNewGame();
+
+            await Clients.All.SendAsync("Equation", CurrentGame.GetCurrentEquation());
+        }
+    }
+
+    public class Equation
+    {
+        Random rnd = new Random();
+
+        public Equation()
+        {
+            x = rnd.Next(1, 100);
+            y = rnd.Next(1, 100);
+            result = x + y;
+            hasWinner = false;
+        }
+
+        public int x { get; }
+        public int y { get; }
+        private int result;
+        public bool hasWinner;
+
+        public int GetResult()
+        {
+            return result;
+        }
+    }
+
+
+}
---
+++ SolveMath/wwwroot/js/solveMath.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/solveMathHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (type, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    switch (type) {
+        case "MSG":
+            document.getElementById("messagesList").appendChild(li);
+            break;
+        case "GAME_RESTARTING":
+            document.getElementById("sendButton").disabled = true;
+            document.getElementById("messagesList").innerHTML = "";
+            document.getElementById("messagesList").appendChild(li);
+            break;
+        case "GAME":
+            document.getElementById("messagesList").appendChild(li);
+            break;
+    }
+});
+
+connection.on("Equation", function (currentEquation) {
+    document.getElementById("sendButton").disabled = false;
+    document.getElementById("messagesList").innerHTML = "";
+    $("#x").html(currentEquation.x);
+    $("#y").html(currentEquation.y);
+    $('#messageInput').val('');
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    connection.invoke("GetCurrentGame");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendAnswer", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
